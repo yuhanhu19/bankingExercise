85028da19ae4dc20bcb6dc9876cb414e37fdb3cb
Yuhan Hu <yuhan.hu@cat.com>
2023-01-01 22:45:08 +0800

    [Yuhan] fix: remove redundant code in test

--- bankingExercise/bankingExercise/Tests.cs
+++ bankingExercise/bankingExercise/Tests.cs
@@ -9,13 +9,11 @@ namespace bankingExercise
         public void ShouldThrowExceptionWhenDepositGiven0OrNegativeAmount()
         {
             var account = new Account();
-            var ex1 = Assert.Throws<Exception>(() => account.deposit(0));
-            account.deposit(0);
-            var ex2 = Assert.Throws<Exception>(() => account.deposit(-2));
-            account.deposit(0);
-
-            Assert.Equal("Deposit amount must be at least 1", ex1.Message);
-            Assert.Equal("Deposit amount must be at least 1", ex2.Message);
+            var ex1 = Assert.Throws<Exception>(() => account.Deposit(0));
+            var ex2 = Assert.Throws<Exception>(() => account.Deposit(-2));
+            
+            Assert.Equal("Deposit amount must be greater than 0", ex1.Message);
+            Assert.Equal("Deposit amount must be greater than 0", ex2.Message);
         }
     }
 }
