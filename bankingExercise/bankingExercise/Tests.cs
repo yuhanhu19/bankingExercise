bf60dd4d80700fbd3e0a93e6f306f18ac007c99a
Yuhan Hu <yuhan.hu@cat.com>
2023-01-01 22:37:10 +0800

    [Yuhan] test: add test for deposit method ShouldThrowExceptionWhenDepositGiven0OrNegativeAmount

---
+++ bankingExercise/bankingExercise/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace bankingExercise
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldThrowExceptionWhenDepositGiven0OrNegativeAmount()
+        {
+            var account = new Account();
+            var ex1 = Assert.Throws<Exception>(() => account.deposit(0));
+            account.deposit(0);
+            var ex2 = Assert.Throws<Exception>(() => account.deposit(-2));
+            account.deposit(0);
+
+            Assert.Equal("Deposit amount must be at least 1", ex1.Message);
+            Assert.Equal("Deposit amount must be at least 1", ex2.Message);
+        }
+    }
+}
