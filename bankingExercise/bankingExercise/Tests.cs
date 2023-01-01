78c043b4ccc035cb189012c9983f68f93be44cc7
Yuhan Hu <yuhan.hu@cat.com>
2023-01-01 23:55:11 +0800

    [Yuhan] test: add test ShouldStoreTransactionSuccessWhenDepositGivenAbove0Amount

--- bankingExercise/bankingExercise/Tests.cs
+++ bankingExercise/bankingExercise/Tests.cs
@@ -15,5 +15,21 @@ namespace bankingExercise
             Assert.Equal("Deposit amount must be greater than 0", ex1.Message);
             Assert.Equal("Deposit amount must be greater than 0", ex2.Message);
         }
+
+        [Fact]
+        public void ShouldStoreTransactionSuccessWhenDepositGivenAbove0Amount()
+        {
+            var account = new Account();
+            var transactionRecStored = new TransactionRec(DateTime.Today.ToString("MM/dd/yyyy"), 100, 100);
+            
+            account.Deposit(100);
+            var recordCount = account.accountTransactionsTable.Count;
+            var transactionRecord  = account.accountTransactionsTable[0];
+            
+            Assert.Equal(1, recordCount);
+            Assert.Equal(transactionRecStored.Date, transactionRecord.Date);
+            Assert.Equal(transactionRecStored.Amount, transactionRecord.Amount);
+            Assert.Equal(transactionRecStored.Balance, transactionRecord.Balance);
+        }
     }
 }
